ae3b2ff641b6ad2eef6cb6ca5ee0ca963a7b5dcb
yoshiyu1209 <86815449+yoshiyu1209@users.noreply.github.com>
2021-08-07 09:33:44 +0900

    itemモデル作成

---
+++ db/migrate/20210807002858_create_items.rb
@@ -0,0 +1,24 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :title,                null: false
+      t.text :text,                   null: false
+      t.date :sale_date,              null: false
+      t.string :weight,               null: false
+      t.integer :area_id,             null: false
+      t.date :catch_day,              null: false
+      t.integer :class_id,            null: false
+      t.string :tighten,              null: false
+      t.string :process,              null: false
+      t.string :process_timing,       null: false
+      t.integer :storage_id,          null: false
+      t.integer :shipping_charges_id, null: false
+      t.integer :days_to_ship_id,     null: false
+      t.integer :delivery_id,         null: false
+      t.integer :shipping_area_id,    null: false
+      t.integer :price,               null: false
+      t.references :user,             null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_133548) do
+ActiveRecord::Schema.define(version: 2021_08_07_002858) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.date "sale_date", null: false
+    t.string "weight", null: false
+    t.integer "area_id", null: false
+    t.date "catch_day", null: false
+    t.integer "class_id", null: false
+    t.string "tighten", null: false
+    t.string "process", null: false
+    t.string "process_timing", null: false
+    t.integer "storage_id", null: false
+    t.integer "shipping_charges_id", null: false
+    t.integer "days_to_ship_id", null: false
+    t.integer "delivery_id", null: false
+    t.integer "shipping_area_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +53,5 @@ ActiveRecord::Schema.define(version: 2021_08_06_133548) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
