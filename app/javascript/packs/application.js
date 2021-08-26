640f8ac0448ee5486bb267ca2365fa19bbf4154b
yoshiyu1209 <86815449+yoshiyu1209@users.noreply.github.com>
2021-08-26 16:37:08 +0900

    application.jsに追記

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,9 @@ require("jquery")
 require('../preview')
 require('../flash_window')
 
+//= stub flash_window
+//= require_tree .
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
